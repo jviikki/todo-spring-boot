c6ab2b58d3a4244fc847427a4df3dca206e20eaf
Jarkko Viikki <jarkko.viikki@gmail.com>
2018-11-22 19:33:13 +0200

    Added exception handling to TaskController

--- src/main/java/com/jviikki/todo/TaskController.java
+++ src/main/java/com/jviikki/todo/TaskController.java
@@ -3,6 +3,7 @@ package com.jviikki.todo;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 // import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,7 +14,7 @@ public class TaskController {
     private TaskService taskService;
 
     @RequestMapping(value = "/api/tasks/{id}", method = RequestMethod.GET)
-    public Task getTask(@PathVariable("id") long id) {
+    public Task getTask(@PathVariable("id") long id) throws TaskService.TaskNotFoundException {
         return taskService.getTask(id);
     }
 
@@ -21,4 +22,17 @@ public class TaskController {
     public void createTask(@RequestBody TaskData taskData) {
         taskService.createTask(new Task(0, taskData.getName(), taskData.getCreatedAt()));
     }
+
+    @ExceptionHandler(TaskService.TaskNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleAppException(TaskService.TaskNotFoundException e) {
+        return "Task not found";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleAppException(Exception e) {
+        return e.getMessage();
+    }
+
 }
--- src/main/java/com/jviikki/todo/TaskService.java
+++ src/main/java/com/jviikki/todo/TaskService.java
@@ -1,6 +1,8 @@
 package com.jviikki.todo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,12 +13,18 @@ public class TaskService {
     private static final ConcurrentMap<Long, Task> todoList = new ConcurrentHashMap<>();
     private final AtomicLong currentId = new AtomicLong();
 
+    // @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class TaskNotFoundException extends Exception {
+    }
+
     public void createTask(Task task) {
         task.setId(currentId.incrementAndGet());
         this.todoList.put(new Long(task.getId()), task);
     }
 
-    public Task getTask(long id) {
-        return this.todoList.get(new Long(id));
+    public Task getTask(long id) throws TaskNotFoundException {
+        Task t = this.todoList.get(new Long(id));
+        if (t == null) throw new TaskNotFoundException();
+        return t;
     }
 }
